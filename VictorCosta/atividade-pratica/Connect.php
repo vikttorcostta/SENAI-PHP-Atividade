45870946a98c35da812b606d96cfd951cb925029
Paulo Victor Costa <paulo.v.melo@ba.estudante.senai.br>
2024-08-27 20:58:59 -0300

    :bricks: Implementação: Conexão com banco de dados

---
+++ VictorCosta/atividade-pratica/Connect.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Connect;
+use PDO;
+
+class Connect extends PDO{
+    
+    private const HOST = 'localhost';
+    private const DATABASE = 'victorsenai';
+    private const USERNAME = 'root';
+    private const PASSWORD = '';
+
+    public function __construct(){
+
+        parent::__construct("mysql:host=".self::HOST.";dbname=".self::DATABASE, self::USERNAME, self::PASSWORD);
+       
+        $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->exec("SET CHARACTER SET utf8");
+        echo "<h2>Conectado ao Banco de Dados MySQL!</h2>";
+
+    }
+
+}
+
+
+$conn = new Connect();
+var_dump($conn);
+
+
+
